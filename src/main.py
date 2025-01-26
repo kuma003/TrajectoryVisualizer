443124e8820e4ad65368dbc939c380766d55dc3f
kuma-1220 <sato.kuma.t7@dc.tohoku.ac.jp>
2025-01-27 02:49:20 +0900

    feat: refactor map data handling and improve UI integration with progress indication

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import time
 import numpy as np
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
-from pyqtgraph.opengl import GLViewWidget
+from pyqtgraph.opengl import GLViewWidget, GLGraphicsItem
 import requests
 from pyqtgraph.Qt import QtCore, QtGui
 from PyQt6.QtCore import pyqtSignal, QThread
@@ -16,27 +16,8 @@ from stl import mesh
 
 from dataclasses import dataclass
 
-from PyQt6.QtWidgets import (
-    QMainWindow,
-    QApplication,
-    QPushButton,
-    QWidget,
-    QVBoxLayout,
-    QSplitter,
-    QLabel,
-    QComboBox,
-    QProgressDialog,
-)
-
-
-@dataclass
-class MapData:
-    """map data"""
-
-    X: np.ndarray
-    Y: np.ndarray
-    Z: np.ndarray
-    color: np.ndarray
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import QPainter, QColor, QFont
 
 
 class MapDataThread(QThread):
@@ -45,12 +26,16 @@ class MapDataThread(QThread):
     signal_progress: pyqtSignal = pyqtSignal(int)
     signal_message: pyqtSignal = pyqtSignal(str)
 
-    def __init__(self, map_viewer: GLViewWidget, map_name: str):
+    def __init__(self, map_name: str):
         super().__init__()
-        self.map_viewer: GLViewWidget = map_viewer
         self.spec: MapSpec = None
         self.urls: list[list[str]] = [[]]
-        self.map = MapData([], [], [], [])
+        self.X: np.ndarray = np.array([])
+        self.Y: np.ndarray = np.array([])
+        self.Z: np.ndarray = np.array([])
+        self.color: np.ndarray = np.array([])
+        self.px_w: float = 0
+        self.px_h: float = 0
 
         self.terminate = False
 
@@ -74,7 +59,7 @@ class MapDataThread(QThread):
             self.spec.zoom,
         )
 
-    def __get_map_data(self):
+    def __get_map_data(self) -> None:
         """get the map data using the API"""
 
         # download the map data
@@ -86,7 +71,7 @@ class MapDataThread(QThread):
             map_arr.append([])
             for u in row:
                 if self.terminate:
-                    return False  # terminate the thread
+                    return  # terminate the thread
                 idx += 1
 
                 # send progress status
@@ -108,69 +93,83 @@ class MapDataThread(QThread):
                     # prevent the server from being overloaded
                     time.sleep(0.001)
 
-        self.map.Z = [np.hstack(sublist) for sublist in map_arr]
-        self.map.Z = np.vstack(self.map.Z)
+        self.Z = [np.hstack(sublist) for sublist in map_arr]
+        self.Z = np.vstack(self.Z)
 
         if self.terminate:
-            return False  # terminate the thread
+            return  # terminate the thread
 
         # self.map.Z = self.map.Z[::5, ::5] # downsample
         lat, lon = self.spec.northwest
-        px_w, px_h = get_px_in_meter(lat=lat, lon=lon, zoom=self.spec.zoom)
+        self.px_w, self.px_h = get_px_in_meter(lat=lat, lon=lon, zoom=self.spec.zoom)
 
-        width = px_w * self.map.Z.shape[0]
-        height = px_h * self.map.Z.shape[1]
+        width = self.px_w * self.Z.shape[0]
+        height = self.px_h * self.Z.shape[1]
 
-        self.map.X = np.linspace(-width / 2, width / 2, self.map.Z.shape[0])
-        self.map.Y = np.linspace(-height / 2, height / 2, self.map.Z.shape[1])
+        self.X = np.linspace(-width / 2, width / 2, self.Z.shape[0])
+        self.Y = np.linspace(-height / 2, height / 2, self.Z.shape[1])
 
         # set the color of the map based on the elevation
-        self.map.color = np.empty(
-            (len(self.map.X), len(self.map.Y), 4), dtype=np.float32
-        )
-        max = np.max(self.map.Z)
-        self.map.color[..., 0] = 0.5
-        self.map.color[..., 1] = np.clip(self.map.Z / max * 0.4 + 0.6, 0, 1)
-        self.map.color[..., 2] = 0.5
-        self.map.color[..., 3] = 1
-        self.map.color[self.map.Z == 0, :] = [0, 0, 1, 1]  # sea color
+        self.color = np.empty((len(self.X), len(self.Y), 4), dtype=np.float32)
+        max = np.max(self.Z)
+        self.color[..., 0] = 0.5
+        self.color[..., 1] = np.clip(self.Z / max * 0.4 + 0.6, 0, 1)
+        self.color[..., 2] = 0.5
+        self.color[..., 3] = 1
+        self.color[self.Z == 0, :] = [0, 0, 1, 1]  # sea color
         # flatten the color array
-        self.map.color = self.map.color.reshape(len(self.map.X) * len(self.map.Y), 4)
-
-        return True
+        self.color = self.color.reshape(len(self.X) * len(self.Y), 4)
 
     def run(self):
         self.terminate = False
 
-        if not self.__get_map_data():  # download the map data
-            return
-
-        self.map_viewer.clear()  # clear the current map data
-
-        self.map_item = gl.GLSurfacePlotItem(
-            x=self.map.X,
-            y=self.map.Y,
-            z=self.map.Z,
-            colors=self.map.color,
-            shader="shaded",
-            smooth=True,
-        )
-
-        self.map_viewer.addItem(self.map_item)
+        self.__get_map_data()  # download the map data
 
 
 class TrajectoryViwer(GLViewWidget):
     def __init__(self):
         super().__init__()
-        self.show()
-        self.setCameraPosition(distance=1500)
 
         self.map_item: gl.GLSurfacePlotItem = None
+        self.paint_item: GLGraphicsItem.GLGraphicsItem = None
+        self.pbar: QProgressDialog = None
+        self.mapDataThread: MapDataThread = None
 
         self.vbl = QVBoxLayout()
         self.setLayout(self.vbl)
-        self.pbar: QProgressDialog = None
-        self.mapDataThread: MapDataThread = None
+
+        rocket_mesh = mesh.Mesh.from_file("src/rocket_model.stl")
+        verts = np.array(rocket_mesh.vectors, dtype=np.float32)
+        faces = np.arange(verts.shape[0] * 3, dtype=np.uint32).reshape(
+            verts.shape[0], 3
+        )
+        # self.model = gl.GLMeshItem(
+        #     vertexes=verts.reshape(-1, 3),
+        #     faces=faces,
+        #     glOptions="opaque",
+        #     shader="shaded",
+        #     color=(1, 1, 1, 1),
+        # )
+        # self.addItem(self.model)
+
+        self.setCameraPosition(distance=1500)
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+
+        if self.mapDataThread is not None:
+            self.painter = QPainter(self)
+            self.painter.setFont(QFont("Arial", 8))
+            self.painter.setPen(QColor(20, 20, 20, 128))
+            attribution = f" Data Attribute: {self.mapDataThread.spec.dataAttribute} "
+            af = (
+                QtCore.Qt.AlignmentFlag.AlignBottom | QtCore.Qt.AlignmentFlag.AlignRight
+            )
+            rect = self.painter.boundingRect(self.rect(), af, attribution)
+            self.painter.fillRect(rect, QColor(255, 255, 255, 128))
+            self.painter.drawText(rect, af, attribution)
+
+            self.painter.end()
 
     def set_button(self, button):
         self.start_button = button
@@ -180,15 +179,15 @@ class TrajectoryViwer(GLViewWidget):
         self.start_button.setEnabled(False)
 
         # start the thread to download the map data
-        self.mapDataThread = MapDataThread(self, map_name)
+        self.mapDataThread = MapDataThread(map_name)
         self.mapDataThread.signal_progress.connect(self.update_progress)
         self.mapDataThread.signal_message.connect(self.update_prog_message)
+        self.mapDataThread.finished.connect(self.draw_map)
         # progress bar initialization
         self.pbar = QProgressDialog("Downloading map data...", "cancel", 0, 100, self)
         self.vbl.addWidget(self.pbar)
         self.pbar.setWindowModality(QtCore.Qt.WindowModality.WindowModal)
         self.mapDataThread.start()
-        self.mapDataThread.setTerminationEnabled(True)
         self.pbar.canceled.connect(self.draw_map_termiante)
 
     def update_progress(self, value):
@@ -203,6 +202,30 @@ class TrajectoryViwer(GLViewWidget):
     def draw_map_termiante(self):
         self.mapDataThread.terminate = True
         self.start_button.setEnabled(True)
+
+    def draw_map(self):
+        if self.mapDataThread.terminate == True:
+            # if the thread is terminated unexpectedly
+            return
+
+        if self.map_item is not None:
+            self.removeItem(self.map_item)  # remove the previous map datas
+
+        self.map_item = gl.GLSurfacePlotItem(
+            x=self.mapDataThread.X,
+            y=self.mapDataThread.Y,
+            z=self.mapDataThread.Z,
+            colors=self.mapDataThread.color,
+            shader="shaded",
+            smooth=True,
+        )
+        self.addItem(self.map_item)
+
+        self.paint_item = GLGraphicsItem.GLGraphicsItem()
+        self.addItem(self.paint_item)
+        painter = QtGui.QPainter(self.paint_item.view())
+        painter.drawText(0, 0, "Hello World")
+        painter.end()
 
 
 class MainWindow(QSplitter):
@@ -223,24 +246,36 @@ class MainWindow(QSplitter):
 
         # side panel settings
         self.side_panel: QWidget = QWidget()
-        self.side_panel.setMaximumWidth(200)
+        self.side_panel.setFixedWidth(200)
         self.addWidget(self.side_panel)
         layout = QVBoxLayout()
         self.side_panel.setLayout(layout)
 
-        # self.map_select = QLabel("Select Map")
-        # layout.addWidget(self.map_select)
+        select_map_layout = QHBoxLayout()
+        layout.addLayout(select_map_layout)
+        self.map_select = QLabel("Select Map")
+        select_map_layout.addWidget(self.map_select)
         self.map_combo = QComboBox()
-        layout.addWidget(self.map_combo)
+        select_map_layout.addWidget(self.map_combo)
         for spec in map_settings.specs:
             self.map_combo.addItem(spec.name)
 
-        self.update_button = QPushButton("Update")
-        layout.addWidget(self.update_button)
+        map_button_layout = QVBoxLayout()
+        layout.addLayout(map_button_layout)
+        self.update_button = QPushButton("Apply Map Selection")
+        self.update_button.setIcon(QtGui.QIcon("src/icons/draw.svg"))
+        map_button_layout.addWidget(self.update_button)
         self.update_button.clicked.connect(
             lambda: self.trajectory_viewer.set_map(self.map_combo.currentText())
         )
-        self.trajectory_viewer.set_button(self.update_button)
+        self.trajectory_viewer.set_button(
+            self.update_button
+        )  # asocciate the button with the viewer
+
+        self.reload_conf_button = QPushButton("Reload Settings File")
+        self.reload_conf_button.setIcon(QtGui.QIcon("src/icons/refresh.svg"))
+        # self.reload_conf_button.setFixedWidth(30)
+        map_button_layout.addWidget(self.reload_conf_button)
 
 
 if __name__ == "__main__":
